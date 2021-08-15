53e37c335597e767e07bebe73ea32b273562e4dc
LiubovHrzhymalo <lubavasns@gmail.com>
2021-08-15 16:42:53 +0300

    Revers

---
+++ unit_4_string/src/main/java/ua/com/alevel/StringMain.java
@@ -0,0 +1,21 @@
+package ua.com.alevel;
+
+import java.util.Scanner;
+
+public class StringMain {
+    public static void main(String[] args) {
+
+        StringRevers stringReverse = new StringRevers();
+        Scanner text = new Scanner(System.in);
+        System.out.print("Please, enter the text ");
+        String a = text.nextLine();
+        System.out.println(stringReverse.reverseOne(a));
+        System.out.println();
+        System.out.print("Please, enter the text ");
+        String b = text.nextLine();
+        System.out.println(stringReverse.reverseTwo(b, true));
+        System.out.println();
+
+
+    }
+}
--- unit_4_string/src/main/java/ua/com/alevel/StringRevers.java
+++ unit_4_string/src/main/java/ua/com/alevel/StringRevers.java
@@ -4,21 +4,26 @@ import java.util.Scanner;
 
 public class StringRevers {
 
-    public static String main(String[] args){
-        Scanner text = new Scanner(System.in);
-
-        System.out.print("Please, enter the text ");
-        String a = text.nextLine();
-
+    public  String reverseOne(String a) {
         char[] array = a.toCharArray();
         String result = "";
         for (int i = array.length - 1; i >= 0; i--) {
             result = result + array[i];
         }
         return result;
-
-
     }
+    public String reverseTwo(String a, boolean reverseTwo) {
+        String[] array = a.split(" ");
+       String result = "";
+        for (int i = 0; i < array.length; i++) {
+           result = result + reverseOne(array[i]) + " ";
+        }
+        return result;
+   }
+
+
+
+
 }
 
 
