a9d3e6e197c208c03a12ab9fde5a4f730656d597
LiubovHrzhymalo <lubavasns@gmail.com>
2021-08-19 01:03:52 +0300

    Revers

--- unit_4_string/src/main/java/ua/com/alevel/StringMain.java
+++ unit_4_string/src/main/java/ua/com/alevel/StringMain.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class StringMain {
     public static void main(String[] args) {
-
         StringRevers stringReverse = new StringRevers();
         Scanner text = new Scanner(System.in);
         System.out.print("Please, enter the text: ");
@@ -15,14 +14,9 @@ public class StringMain {
         System.out.println(stringReverse.reverseTwo(b, true));
         System.out.print("Please, enter the text: ");
         String c = text.nextLine();
-        System.out.print("Please, enter the text2: ");
+        System.out.print("Please, enter the dest-text : ");
         String d = text.nextLine();
-        System.out.println(stringReverse.reverseThree (c,d));
-
-
-
-
-
+        System.out.println(stringReverse.reverseThree(c, d));
 
 
     }
--- unit_4_string/src/main/java/ua/com/alevel/StringRevers.java
+++ unit_4_string/src/main/java/ua/com/alevel/StringRevers.java
@@ -1,10 +1,9 @@
 package ua.com.alevel;
 
 public class StringRevers {
-
     private String dest;
 
-    public  String reverseOne(String a) {
+    public String reverseOne(String a) {
         char[] array = a.toCharArray();
         String result = "";
         for (int i = array.length - 1; i >= 0; i--) {
@@ -12,31 +11,31 @@ public class StringRevers {
         }
         return result;
     }
+
     public String reverseTwo(String a, boolean reverseTwo) {
         String[] array = a.split(" ");
-       String result = "";
-        for (int i = 0; i < array.length; i++) {
-           result = result + reverseOne(array[i]) + " ";
-        }
-        return result;
-   }
-
-    public  String reverseThree(String c, String a) {
-       String reversedDest = reverseDest(dest);
-        String result = a.replace(dest, reversedDest);
-        return result;
-}
-    String reverseDest(String dest) {
-        int stringLength = dest.length();
         String result = "";
-        for (int i = 0; i < stringLength; i++) {
-            result = dest.charAt(i) + result;
+        for (int i = 0; i < array.length; i++) {
+            result = result + reverseOne(array[i]) + " ";
         }
         return result;
     }
 
+    public String reverseThree(String text, String dest) {
+        String reversedDest = reverseDest(dest);
+        String result = text.replace(dest, reversedDest);
+        System.out.println(result);
+        return result;
+    }
 
-
+    public String reverseDest(String dest) {
+        int stringLength = dest.length();
+        String word = "";
+        for (int i = 0; i < stringLength; i++) {
+            word = dest.charAt(i) + word;
+        }
+        return word;
+    }
 
 
 }
